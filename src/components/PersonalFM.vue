fda3f68e943b063372d3cc1a7e3072b0d618f2f4
Fu Xingyu <1253076882@qq.com>
2021-09-20 14:40:24 +0800

    为FM容器添加最小高度，以适应FMCard组件

--- src/components/PersonalFM.vue
+++ src/components/PersonalFM.vue
@@ -1,45 +1,87 @@
 <template>
-  <div class="FM" :style="{
-    backgroundImage: `url(${PriFM ? PriFM.coverImgUrl : fallBackCoverImgUrl})`,
-  }">
-    <div class="cover">
-      <img :src="PriFM ? PriFM.coverImgUrl : fallBackCoverImgUrl" alt="">
-    </div>
-    <div class="song-info">
-      <span>个性电台</span>
-      <span>{{ PriFM ? PriFM.name : fallBackName}}</span>
-      <span>{{ PriFM ? PriFM.artists : fallBackArtists }}</span>
-      <div class="controls">
-        <v-icon @click="next" name="step-forward" :scale="size" title="手气不错?"/>
-        <v-icon @click="like" name="regular/heart" :scale="size" title="喜欢"/>
-        <v-icon @click="dislike" name="heart-broken" :scale="size" title="不喜欢"/>
-        <v-icon @click="showComment" name="comment-dots" :scale="size" title="评论"/>
+  <div :style="FMContainer">
+    <div
+      class="FM"
+      :style="{
+        backgroundImage: `url(${
+          PriFM ? PriFM.coverImgUrl : fallBackCoverImgUrl
+        })`,
+      }"
+    >
+      <div class="cover">
+        <img :src="PriFM ? PriFM.coverImgUrl : fallBackCoverImgUrl" alt="" />
+      </div>
+      <div class="song-info">
+        <span>个性电台</span>
+        <span>{{ PriFM ? PriFM.name : fallBackName }}</span>
+        <span>{{ PriFM ? PriFM.artists : fallBackArtists }}</span>
+        <div class="controls">
+          <v-icon
+            @click="next"
+            name="step-forward"
+            :scale="size"
+            title="手气不错?"
+            :style="{
+              cursor: 'pointer',
+            }"
+          />
+          <v-icon
+            @click="like"
+            name="regular/heart"
+            :scale="size"
+            title="喜欢"
+            :style="{
+              cursor: 'pointer',
+            }"
+          />
+          <v-icon
+            @click="dislike"
+            name="heart-broken"
+            :scale="size"
+            title="不喜欢"
+            :style="{
+              cursor: 'pointer',
+            }"
+          />
+          <v-icon
+            @click="showComment"
+            name="comment-dots"
+            :scale="size"
+            title="评论"
+            :style="{
+              cursor: 'pointer',
+            }"
+          />
+        </div>
       </div>
     </div>
+    <FMCard/>
   </div>
-
 </template>
 
 <script>
-import 'vue-awesome/icons';
-import Icon from 'vue-awesome/components/Icon';
-import request from '@/request/request'
-import {parseSongInfo} from '@/utils'
-import {UPDATE_CURRENT_PLAY} from '@/store/actionType';
-import {mapMutations} from 'vuex';
+import "vue-awesome/icons";
+import Icon from "vue-awesome/components/Icon";
+import request from "@/request/request";
+import { parseSongInfo } from "@/utils";
+import { UPDATE_CURRENT_PLAY } from "@/store/actionType";
+import { mapMutations } from "vuex";
+import FMCard from './FMCard.vue';
 
 export default {
   name: "PersonalFM",
   components: {
-    'v-icon': Icon,
+    "v-icon": Icon,
+    FMCard,
   },
   data() {
     return {
-      size: '1.6',
+      size: "1.6",
       // 未加载时的备用 cover-img-url
-      fallBackCoverImgUrl: 'https://p2.music.126.net/wtiI4Tzal-1ynR-Z700pfg==/109951165741124381.jpg',
-      fallBackName: '目及皆是你',
-      fallBackArtists: '小蓝背心',
+      fallBackCoverImgUrl:
+        "https://p2.music.126.net/wtiI4Tzal-1ynR-Z700pfg==/109951165741124381.jpg",
+      fallBackName: "目及皆是你",
+      fallBackArtists: "小蓝背心",
       PriFM: null,
 
       // 当前音乐的id
@@ -50,27 +92,29 @@ export default {
 
       // 当前索引, 越界后会重新请求
       playListIndex: 0,
-    }
+    };
   },
   methods: {
     ...mapMutations({
       updateCurrentPlay: UPDATE_CURRENT_PLAY,
     }),
     like() {
-      console.log('I like this music');
+      console.log("I like this music");
     },
     dislike() {
-      console.log('I dislike this music');
+      console.log("I dislike this music");
     },
     showComment() {
-      console.log('comment is loading...');
+      console.log("comment is loading...");
     },
     next() {
       // 先判断一下，currentPlayList 拿完后再重新请求
       const currentPlayList = this.currentPlayList;
       const index = this.playListIndex;
       // 如果拿完了或者 currentPlayList为空, 就要重新请求
-      const reRequest = currentPlayList ? currentPlayList.length <= index : true;
+      const reRequest = currentPlayList
+        ? currentPlayList.length <= index
+        : true;
 
       if (!reRequest) {
         // 不用重新请求
@@ -78,68 +122,79 @@ export default {
         this.currentPlayId = this.PriFM.id;
         return;
       }
-      request.get('/personal_fm')
-          .then(({data}) => {
-            if (data.code === 200) {
-              this.currentPlayList = data.data.map(item => {
-                return {
-                  name: item.name,
-                  id: item.id,
-                  artists: item.artists.map(item => item.name).join('/'),
-                  coverImgUrl: item.album.picUrl,
-                }
-              });
+      request
+        .get("/personal_fm")
+        .then(({ data }) => {
+          if (data.code === 200) {
+            this.currentPlayList = data.data.map((item) => {
+              return {
+                name: item.name,
+                id: item.id,
+                artists: item.artists.map((item) => item.name).join("/"),
+                coverImgUrl: item.album.picUrl,
+              };
+            });
 
-              this.PriFM = this.currentPlayList[0];
-              this.currentPlayId = this.PriFM.id;
-              this.playListIndex  = 1;
-            } else {
-              this.$message({
-                message: `${data.msg}, 状态码: ${data.code}`,
-                type: 'info'
-              })
-            }
-          }).catch(err => {
-        this.$message({
-          message: '发生错误, 请到控制面板查看',
-          type: 'error'
+            this.PriFM = this.currentPlayList[0];
+            this.currentPlayId = this.PriFM.id;
+            this.playListIndex = 1;
+          } else {
+            this.$message({
+              message: `${data.msg}, 状态码: ${data.code}`,
+              type: "info",
+            });
+          }
         })
-        console.group('PersonalFM methods');
-        console.log(err);
-        console.groupEnd('PersonalFM methods');
-      })
-
-    }
+        .catch((err) => {
+          this.$message({
+            message: "发生错误, 请到控制面板查看",
+            type: "error",
+          });
+          console.group("PersonalFM methods");
+          console.log(err);
+          console.groupEnd("PersonalFM methods");
+        });
+    },
   },
   watch: {
     currentPlayId(id) {
-      request.get('/song/detail', {
-        params: {
-          ids: id,
-        }
-      }).then(({data}) => {
-        if (data.code === 200) {
-          const playList = data.songs.map(parseSongInfo);
-          // 保存到 store
-          this.updateCurrentPlay(playList[0]);
-        } else {
-          this.$message({
-            message: `${data.msg}, 状态码: ${data.code}`,
-            type: 'info'
-          })
-        }
-      }).catch(err => {
-        this.$message({
-          message: '发生错误, 请到控制面板查看',
-          type: 'error'
+      request
+        .get("/song/detail", {
+          params: {
+            ids: id,
+          },
         })
-        console.group('PlayList loadRest');
-        console.log(err);
-        console.groupEnd('PlayList loadRest');
-      })
+        .then(({ data }) => {
+          if (data.code === 200) {
+            const playList = data.songs.map(parseSongInfo);
+            // 保存到 store
+            this.updateCurrentPlay(playList[0]);
+          } else {
+            this.$message({
+              message: `${data.msg}, 状态码: ${data.code}`,
+              type: "info",
+            });
+          }
+        })
+        .catch((err) => {
+          this.$message({
+            message: "发生错误, 请到控制面板查看",
+            type: "error",
+          });
+          console.group("PlayList loadRest");
+          console.log(err);
+          console.groupEnd("PlayList loadRest");
+        });
     },
+  },
+  computed: {
+    FMContainer() {
+      return {
+        minHeight: '380px',
+      }
+    }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -149,7 +204,7 @@ $radius: 15px;
 $cover-size: 200px;
 $fm-color: #fffafab3;
 .FM {
-  @include flex-layout('row');
+  @include flex-layout("row");
   justify-content: space-around;
   background-repeat: no-repeat;
   background-size: cover;
@@ -194,7 +249,7 @@ $fm-color: #fffafab3;
 }
 
 .song-info {
-  @include flex-layout('column');
+  @include flex-layout("column");
   position: relative;
   justify-content: space-between;
   align-items: flex-start;
@@ -232,7 +287,7 @@ $fm-color: #fffafab3;
 }
 
 .controls {
-  @include flex-layout('row');
+  @include flex-layout("row");
   justify-content: space-between;
   width: $cover-size;
   color: #bef6db;
