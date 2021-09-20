86b0286f8b21edf167311bf71fb260cb5fb7c732
Fu Xingyu <1253076882@qq.com>
2021-09-20 15:37:39 +0800

    移除静态数据

--- src/components/UserFavoritePlayList.vue
+++ src/components/UserFavoritePlayList.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="user-favorite-play-list">
-    <div v-for="music of playList" @click="goToPlayList(music.id)" :key="music.id">
-      <figure >
-        <img :src="music.coverImgUrl" alt="" class="cover-img">
+    <div
+      v-for="music of playList"
+      @click="goToPlayList(music.id)"
+      :key="music.id"
+    >
+      <figure>
+        <img :src="music.coverImgUrl" alt="" class="cover-img" />
         <figcaption>
-          {{music.name}}
+          {{ music.name }}
         </figcaption>
       </figure>
     </div>
@@ -12,24 +16,56 @@
 </template>
 
 <script>
-import {playList} from '@/testData/user_favorite_music';
+import request from "@/request/request.js";
+import {mapGetters} from 'vuex';
 export default {
   name: "UserFavoritePlayList",
   data() {
     return {
-      playList: playList.playlist,
-    }
+      playList: null,
+    };
   },
   methods: {
     goToPlayList(id) {
       this.$emit("goToPlayList", id);
-    }
-  }
+    },
+  },
+  computed: {
+    ...mapGetters(['getUserInfo']),
+  },
+  created() {
+    // TODO 这个uid 后期用getUserInfo 从 store 中获取
+    request.get("/user/playlist?uid=245947021")
+      .then(({ data }) => {
+        if (data.code === 200) {
+          this.playList = data.playlist.map(item => {
+            return {
+              id: item.id,
+              coverImgUrl: item.coverImgUrl,
+              name: item.name,
+            }
+          });
+        } else {
+          this.$message({
+            message: `${data.msg}, 状态码: ${data.code}`,
+            type: "info",
+          });
+        }
+      })
+      .catch((err) => {
+        this.$message({
+          message: "发生错误, 请到控制面板查看",
+          type: "error",
+        });
+        console.group("PlayList loadRest");
+        console.log(err);
+        console.groupEnd("PlayList loadRest");
+      });
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-
 .user-favorite-play-list {
   margin-top: 20px;
   display: grid;
@@ -39,10 +75,10 @@ export default {
   figure {
     //overflow: hidden;
     cursor: pointer;
-    transition: .5s;
+    transition: 0.5s;
     position: relative;
     img {
-      transition: .5s;
+      transition: 0.5s;
     }
     &:hover > img {
       transform: translateY(-20px) scale(1.2);
