d61e708f10281c3bbede9339f404e9fb509f1b8e
Fu Xingyu <1253076882@qq.com>
2021-09-22 20:30:21 +0800

    还是无法修复，换了个思路，使用transform改变.recommend的位置

--- src/views/My.vue
+++ src/views/My.vue
@@ -1,87 +1,84 @@
 <template>
-  <div style="height: 200vh">
-    <div class="banner" ref="banner" :style="dynamicHeight">
-      <img class="avatar" alt="avatar">
-      <div class="user-profile">
-        <h1>帮主我们唐门不能群龙无首啊</h1>
-        <ul>
-          <li>
-            <a href="#">
-              <strong>0</strong>
-              <span>关注</span>
-            </a>
-          </li>
-          <li>
-            <a href="">
-              <strong>0</strong>
-              <span>粉丝</span>
-            </a>
-          </li>
-        </ul>
+  <div style="height: 2000px">
+    <div>
+      <div class="banner" id="banner">
+        <img class="avatar" alt="avatar" />
+        <div class="user-profile">
+          <h1>帮主我们唐门不能群龙无首啊</h1>
+          <ul>
+            <li>
+              <a href="#">
+                <strong>0</strong>
+                <span>关注</span>
+              </a>
+            </li>
+            <li>
+              <a href="">
+                <strong>0</strong>
+                <span>粉丝</span>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-    <div class="recommend">
-      <h3>Hi,帮助我们唐门不能群龙无首啊</h3>
-      <h5>今日为你推荐</h5>
-      <el-tabs >
-        <el-tab-pane label="用户管理" name="first">今日推荐</el-tab-pane>
-        <el-tab-pane label="配置管理" name="second">私人FM</el-tab-pane>
-        <el-tab-pane label="角色管理" name="third">我的歌单</el-tab-pane>
-        <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
-      </el-tabs>
+
+      <div class="recommend">
+        <h3>Hi,帮助我们唐门不能群龙无首啊</h3>
+        <h5>今日为你推荐</h5>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import {throttle} from '@/utils';
+// import {throttle} from '@/utils';
+function updateHeight() {
+  let recommend = document.querySelector(".recommend");
+  let referenceHeight = document.querySelector('.banner').clientHeight;
+  let start = 0;
+  let end = 200;
+  let rate = window.pageYOffset / (end - start);
+  let offset = rate * referenceHeight;
+  recommend.style.transform = `translateY(-${offset}px)`;
+}
 
 export default {
   name: "My",
   data() {
     return {
-      bannerHeight: 0,
-      offsetY: 0,
-    }
+      activeName: "second",
+    };
   },
   methods: {
-    setOffsetY: throttle(function () {
-      this.offsetY = window.pageYOffset;
-    }, 50),
-  },
-  computed: {
-    dynamicHeight() {
-      let start = 0;
-      let end = 200;
-      if (this.bannerHeight === 0) {
-        return;
-      }
-      let rate = (end - this.offsetY) / (end - start);
-      let result = rate * this.bannerHeight;
-      if (result <= 0) {
-        return {
-          height: 0 + 'px',
-        }
-      }
-      return {
-        height: result + 'px',
-      }
-    }
+    handleClick(tab, event) {
+      console.log(tab, event);
+    },
   },
   // ↓ ↓ ↓ ↓ ↓ 生命周期 ↓ ↓ ↓ ↓ ↓ ↓
   mounted() {
-    window.addEventListener('scroll', this.setOffsetY);
-    this.bannerHeight = this.$refs.banner.clientHeight;
+    window.addEventListener("scroll", updateHeight);
   },
   beforeDestroy() {
-    window.removeEventListener('scroll', this.setOffsetY);
-  }
+    window.removeEventListener("scroll", updateHeight);
+  },
   // ↑ ↑ ↑ ↑ ↑ 生命周期 ↑ ↑ ↑ ↑ ↑ ↑
-
-}
+};
 </script>
 
 <style lang="scss" scoped>
+.recommend {
+  background: white;
+}
 .banner {
   margin-top: 10px;
   background-image: url("../image/music-player.jpg");
@@ -92,7 +89,7 @@ export default {
   background-position: center top;
   position: relative;
   overflow: hidden;
-  transition: height .2s;
+  transition: height 0.2s;
 
   .avatar {
     content: url("../image/avatar-fallback.jpg");
@@ -127,7 +124,7 @@ export default {
         color: white;
 
         & > strong {
-          transition: .5s;
+          transition: 0.5s;
           font-size: 1.3em;
         }
 
@@ -142,5 +139,4 @@ export default {
     }
   }
 }
-
 </style>
